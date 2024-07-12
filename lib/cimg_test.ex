9e6f6b74c619725ed0b5ef4a142adf273a993938
Luzian Wild <luzian.wild@gmail.com>
2024-07-12 17:10:18 +0200

    Add minimal elixir 1.15.8 project + CircleCI config

---
+++ lib/cimg_test.ex
@@ -0,0 +1,18 @@
+defmodule CimgTest do
+  @moduledoc """
+  Documentation for `CimgTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CimgTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cimg_test_test.exs
@@ -0,0 +1,8 @@
+defmodule CimgTestTest do
+  use ExUnit.Case
+  doctest CimgTest
+
+  test "greets the world" do
+    assert CimgTest.hello() == :world
+  end
+end
